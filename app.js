2c48ccae07d51bbd5c5dd3d2d2c4358b9ce46a87
NAP-GHJ <18052095653@189.cn>
2016-11-07 20:09:21 +0800

    终于搞定了文件上传，记录一下

--- app.js
+++ app.js
@@ -4,9 +4,12 @@
 var express = require('express');
 var app = express();
 
+var fs = require('fs');
 var path = require('path');
 
 /*中间件*/
+
+
 
 /*中间件morgan：在控制台显示req请求信息*/
 var logger = require('morgan');
@@ -16,11 +19,85 @@ app.use(logger('dev'));
 var favicon = require('serve-favicon');
 app.use(favicon('./favicon.ico'));
 
+var bodyParser = require('body-parser');
+app.use(bodyParser.json({limit: '50mb'}));
+app.use(bodyParser.urlencoded({limit: '50mb', extended: true}));
+
 var cors = require('cors');
 app.use(cors());
 
-var cookieParse = require('cookie-parser');
-app.use(cookieParse());
+/*文件上传*/
+// app.use(function (req, res, next) {
+//     res.setHeader('Access-Control-Allow-Origin', 'http://localhost:8080');
+//     res.setHeader('Access-Control-Allow-Methods', 'POST');
+//     res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+//     res.setHeader('Access-Control-Allow-Credentials', true);
+//     next();
+// });
+
+
+var multer = require('multer');
+// var DIR = './public/upload/';
+// var options = {
+//     dest:DIR,
+//     rename:
+// }
+
+// var storage = multer.diskStorage({
+//     destination: function (req, file, cb) {
+//         cb(null, './public/upload/')
+//     },
+//     filename: function (req, file, cb) {
+//         cb(null, req.params.name);
+//     }
+// })
+//
+// var upload = multer({ storage: storage }).any();
+//var upload = multer({dest : DIR}).any();
+
+// var upload = multer({
+//     dest: DIR,
+//     rename: function (fieldname, filename) {
+//         return filename + Date.now();
+//     },
+//     onFileUploadStart: function (file) {
+//         console.log(file.originalname + ' is starting ...');
+//     },
+//     onFileUploadComplete: function (file) {
+//         console.log(file.fieldname + ' uploaded to  ' + file.path);
+//     }
+// }).any();
+
+var storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+        cb(null, './public/upload/')
+    },
+    filename: function (req, file, cb) {
+        cb(null, req.params.name);
+    }
+})
+
+var upload = multer({ storage: storage }).any();
+
+app.post('/upload/:name', function (req, res) {
+    //res.end(req.params.name);
+    //console.log(req.files[0])
+
+    upload(req, res, function (err) {
+        if (err) {
+            console.log(err.toString())
+            return res.end(err.toString());
+        }
+        res.end('File is uploaded');
+    });
+    //console.log(req);
+    //console.log(req);
+    // if(!req.file) {
+    //     return res.status(404)
+    // }
+    // var filename = req.file.filename;
+    // res.status(200).json({filename: filename})
+});
 
 /*路由*/
 var route = require('./route');
@@ -29,7 +106,7 @@ app.use('/api',apiRoute);
 app.use('/',route);
 
 /*静态资源*/
-app.use(express.static('public'));
+app.use(express.static('./public'));
 
 /*错误处理*/
 
--- start.sh
+++ start.sh
@@ -1,1 +1,1 @@
-node ./bin/www
+sudo nodemon ./bin/www
